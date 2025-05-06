29bbf6f6427b3c39f5586a87615ec4ce56841ac3
hrkh <hrkhjp@gmail.com>
2025-05-06 10:52:01 +0900

    Add hamburger menu toggle functionality and restructure header layout for improved navigation

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,4 +1,13 @@
 document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
+  if (hamburger){
+    hamburger.addEventListener("click", function(event) {
+      event.preventDefault();
+      let menu = document.querySelector("#navbar-menu");
+      menu.classList.toggle("collapse");
+    });
+  }
+
   let account = document.querySelector("#account");
   if (account) {
     account.addEventListener("click", function(event) {
