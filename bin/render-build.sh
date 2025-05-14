0d7b27e8d9a1db29415989a64a5e939821d70c2a
hrkh <hrkhjp@gmail.com>
2025-05-14 21:47:52 +0900

    Refactor database reset commands in render-build script for consistency

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,5 @@ set -xo errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
 bundle exec rails db:seed
